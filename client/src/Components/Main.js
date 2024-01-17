f6197a444515387fc315334e060f217fbea0dd44
Pięćdziesiątka Trygonometryczna <dawidkalinowski243@gmail.com>
2024-01-17 22:28:28 +0100

    dodano logikę aby gracze stawiali pinoki naprzemiennie, oraz zmieniono ich kolory

--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -1,23 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Main.css";
 
-const Cell = ({ handleCellClick, id, text }) => {
+const Cell = ({ handleCellClick, id, text, color }) => {
   return (
-    <div id={id} className="cell" onClick={handleCellClick}>
+    <div id={id} className={`cell ${color}`} onClick={handleCellClick}>
       {text}
     </div>
   );
 };
 
-const Main = () => {
+const Main = ({ socket, roomCode }) => {
   const boardSize = 15;
-  const initialBoard = Array(boardSize * boardSize).fill("");
+  const initialBoard = Array(boardSize * boardSize).fill({ value: "", color: "" });
   const [board, setBoard] = useState(initialBoard);
+  const [canPlay, setCanPlay] = useState(true);
+
+  useEffect(() => {
+    socket.on("updateGame", (id) => {
+      console.log("use Effect", id);
+      setBoard((data) => ({ ...data, [id]: { value: "O", color: "white" } }));
+      setCanPlay(true);
+    });
+
+    return () => socket.off("updateGame");
+  });
 
   const handleCellClick = (e) => {
     const id = e.currentTarget.id;
-    if (board[id] == "") {
-      setBoard((data) => ({ ...data, [id]: "X" }));
+    if (canPlay && board[id].value == "") {
+      setBoard((data) => ({ ...data, [id]: { value: "O", color: "black" } }));
+      socket.emit("play", { id, roomCode });
+      setCanPlay(false);
     }
   };
 
@@ -27,7 +40,7 @@ const Main = () => {
         {Array(boardSize).fill().map((_, i) => (
           Array(boardSize).fill().map((_, j) => {
             const id = i * boardSize + j;
-            return <Cell handleCellClick={handleCellClick} id={id.toString()} text={board[id]} />
+            return <Cell handleCellClick={handleCellClick} id={id.toString()} text={board[id].value} color={board[id].color} />
           })
         ))}
       </section>
