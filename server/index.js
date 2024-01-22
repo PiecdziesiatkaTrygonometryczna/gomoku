d5098fa04e25e2f2c1540345d748d58cfade6f09
Pięćdziesiątka Trygonometryczna <dawidkalinowski243@gmail.com>
2024-01-22 15:21:21 +0100

    polaczono z serwerem

--- server/index.js
+++ server/index.js
@@ -5,6 +5,26 @@ const server = http.createServer(app);
 const port = 3003;
 const io = require("socket.io")(server, { cors: { origin: "*" } });
 
+const { MongoClient, ObjectId } = require("mongodb");
+const { v4: uuidv4 } = require("uuid");
+const crypto = require("crypto");
+
+class DBActions {
+  constructor() {
+    const ipAdress = "127.0.0.1";
+    const PORT = "27777";
+    const databaseName = "gomoku";
+    const connectionString = `mongodb://${ipAdress}:${PORT}/${databaseName}`;
+    this.client = new MongoClient(connectionString);
+    this.conn = null;
+  }
+}
+
+
+
+
+
+
 io.on("connection", (socket) => {
   console.log("Ktoś dołączył");
 
